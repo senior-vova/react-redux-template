7390aa3c866369b1354eb15e047bcb7860f1f372
senior-vova <vladimir.reg.acc@gmail.com>
2022-02-26 07:45:17 +0400

    Remove guard system

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router";
-import { AuthGuard } from "./guards";
 import { HomePage, NotFoundPage } from "./pages";
-import { Guard } from "./utils/guard";
 import { MainAppHoc } from "./utils/main.hoc";
 
 const App = () => {
   return (
     <Routes>
-      <Route path={"/profile"} element={Guard(<HomePage />, [AuthGuard])} />
+      <Route path={"/profile"} element={<HomePage />} />
       <Route path={"/auth"} element={<HomePage />} />
       <Route path={"/"} element={<HomePage />} />
       <Route path="*" element={<NotFoundPage />} />
