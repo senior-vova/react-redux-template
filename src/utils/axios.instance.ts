0d04039699aec2b5788758746898aec229f71ae4
senior-vova <vladimir.reg.acc@gmail.com>
2022-06-20 11:18:42 +0400

    Add environment system

--- src/utils/axios.instance.ts
+++ src/utils/axios.instance.ts
@@ -1,11 +1,8 @@
 import axios from "axios";
 import { token } from "./localstorage.token";
 
-const PROD_API_URL = "";
-const DEV_API_URL = "";
-
 export const baseUrl: string =
-  process.env.NODE_ENV === "production" ? PROD_API_URL : DEV_API_URL;
+  process.env.REACT_APP_SERVER_URL || "http://localhost:3333/";
 
 export const instance = axios.create({
   baseURL: `${baseUrl}`,
